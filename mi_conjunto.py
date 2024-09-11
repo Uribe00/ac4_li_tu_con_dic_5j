845433c441ecc2fbf4d8d3212cf33e73ab96df89
Daniel Uribe <a.22308051281323@cbtis128.edu.mx>
2024-09-11 07:37:28 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo de conjunto set")
+tipodeportes={"futbol","Tenis","Basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+print(tipodeportes)
+for Uribe in tipodeportes:
+    print(Uribe)
